6f0f40d2c45617d0ba32c21805bb9fc0e4dd4116
milu234 <milanhazra234.mh@gmail.com>
2019-01-20 17:56:12 +0530

    App Got some errors

---
+++ Views/RootNavigator.js
@@ -0,0 +1,15 @@
+'use strict';
+import React, {
+    Navigator,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class RootNavigator extends React.Component {
+    render() {
+        return <View />
+    }
+
+}
